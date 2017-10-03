0fd0897f294c6922f31354da963c4c12d0f9d17c
herman-hellenes <herman.hellenes@gmail.com>
2017-10-03 15:45:26 +0200

    Update ImporterExporterContract.sol

--- ImporterExporterContract.sol
+++ ImporterExporterContract.sol
@@ -39,10 +39,12 @@ contract ImporterExporterContract {
 
    uint public lol;
 
-  // dictionary that maps addresses to balances
-  mapping (address => uint) private balances;
-  // "private" means that other contracts can't directly query balances
-  // but data is still viewable to other parties on blockchain
+  // Balances for each account
+  mapping (address => uint) balances;   // dictionary that maps addresses to balances
+
+ // What is the balance of a particular account?
+ function balanceOf(address _owner) constant returns (uint256 balance) {
+         return balances[_owner];}
 
   // Names; just saving company names and country
   // setNames is a mapping from uint256 to Names (the struct below). 
@@ -55,6 +57,33 @@ contract ImporterExporterContract {
     string name_importer_company;
     string importer_country;
   }
+      // Owner of account approves the transfer of an amount to another account
+     mapping(address => mapping (address => uint256)) allowed;
+     
+     
+     // Send _value amount of tokens from address _from to address _to
+     // The transferFrom method is used for a withdraw workflow, allowing contracts to send
+     // tokens on your behalf, for example to "deposit" to a contract address and/or to charge
+     // fees in sub-currencies; the command should fail unless the _from account has
+     // deliberately authorized the sender of the message via some mechanism; we propose
+     // these standardized APIs for approval:
+     function transferFrom(
+         address _from,
+         address _to,
+         uint256 _amount
+     ) returns (bool success) {
+         if (balances[_from] >= _amount
+             && allowed[_from][msg.sender] >= _amount
+             && _amount > 0
+             && balances[_to] + _amount > balances[_to]) {
+             balances[_from] -= _amount;
+             allowed[_from][msg.sender] -= _amount;
+             balances[_to] += _amount;
+             return true;
+         } else {
+             return false;
+         }
+     }
   
   // This is the constructor!
   function ImporterExporterContract() {
